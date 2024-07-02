feb34a06ccdbf8ae30b53d64b556abe1f3b3ec8b
Erkin070 <erkinbaev070@mail.ru>
2024-07-02 18:50:13 +0600

    commit

---
+++ src/Medic.java
@@ -0,0 +1,16 @@
+
+import kg.geeks.game.logic.RPG_Game;
+import kg.geeks.game.players.Boss;
+import kg.geeks.game.players.Hero;
+import kg.geeks.game.players.SuperAbility;
+
+public class Medic extends Hero {
+    public Medic (int health, int damage, String name) { super(health, damage, name, SuperAbility.HEAL);{
+    }
+
+
+
+
+
+
+
